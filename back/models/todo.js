f49fd3daf1c638cd75684a10671067d326fef4d9
Victor <vzabrovskiu@gmail.com>
2021-02-02 10:55:39 +0300

    back for addTodo

--- back/models/todo.js
+++ back/models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   body: { type: String, required: true },
-  done: {type: String, default: true},
+  done: {type: String, default: false},
   contact: {type: mongoose.Schema.Types.ObjectId,
     ref: 'user',},
 });
--- back/routes/todo.js
+++ back/routes/todo.js
@@ -8,4 +8,23 @@ router.get('/', async (req, res, next) => {
 
 });
 
+router.post('/addtodo', async (req, res, next) => {
+    const {token} = req.body;
+    let data = jwt.verify(token, tokenKey, (err, decoded)=> {
+        if(err)  res.json({success: false, message: 'token expired'});
+        return decoded
+    })
+    const {id} = data;
+    let user = await User.findOne({_id:id});
+    const {contactId, body} = req.body;
+    const todo = new Todo({
+        body,
+        contact: contactId,
+    })
+    await todo.save();
+    user.todos.push(todo._id);
+    await user.save();
+    res.json({success: true, user})
+});
+
 module.exports = router;
