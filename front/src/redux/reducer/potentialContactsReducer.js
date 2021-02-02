951d284412ec63e7564f77a168e3ef4d0510290a
Anna <anya.mezentseva59@yandex.ru>
2021-02-02 18:28:36 +0300

    New reducers

---
+++ front/src/redux/reducer/potentialContactsReducer.js
@@ -0,0 +1,11 @@
+import { GET_POTENTIAL_CONTACTS } from '../actionTypes';
+
+export const potentialContactsReducer = (state, action) => {
+  switch (action.type) {
+    case GET_POTENTIAL_CONTACTS:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+}
