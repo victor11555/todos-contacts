e9ad14cc46a9b1c97127726487f35d1eb12545c4
Anna <anya.mezentseva59@yandex.ru>
2021-02-02 18:55:47 +0300

    all users search beta

--- front/src/Pages/ContentPage/ContentPage.js
+++ front/src/Pages/ContentPage/ContentPage.js
@@ -1,53 +1,57 @@
-import React, {useEffect} from 'react';
-import {Button, Col, Container, Form, ListGroup, Row} from 'react-bootstrap'
+import React, { useEffect } from 'react';
+import { Button, Col, Container, Form, ListGroup, Row } from 'react-bootstrap'
 import Select from 'react-dropdown-select'
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Userlist from "../../UserList/Userlist";
-import {addContactAc, addToDoAc, getProfileAC} from "../../redux/actionCreators";
+import { addContactAc, addToDoAc, getProfileAC, allUsersAC } from "../../redux/actionCreators";
 
-function ContentPage(props) {
+function ContentPage() {
     const dispatch = useDispatch()
+
 
     useEffect(() => {
         dispatch(getProfileAC())
+        dispatch(allUsersAC())
+        //  dispatch() Фира, солнышко, это тебе
     }, [])
 
     const user = useSelector(state => state.user)
+    const allUsers = useSelector(state => state.allUsers)
     let values = []
-    let users = []
+    let values2 = []
     useEffect(() => {
         if (user.isLogged) {
             user.user.contacts.map(el => {
-                values.push({label: `${el.name} : ${el.phone}`, value: el._id})
+                values.push({ label: `${el.name} : ${el.phone}`, value: el._id })
             })
         }
-        // if(contacts){
-            // contacts.map(el => {
-            //     users.push({label: `${el.name} : ${el.phone}`, value: el._id})
-            // })
-        // }
-    }, [user.isLogged,user.user.contacts])
+        if (allUsers) {
+            allUsers.map(el => {
+                values2.push({ label: `${el.name} : ${el.phone}`, value: el._id })
+            })
+        }
+    }, [user.isLogged, user.user.contacts])
 
     const handleSubmitAddContact = (e) => {
         e.preventDefault()
-        const {contacts} = e.target
-        let contactId = users.filter(el=> el.label == contacts.value)
-        dispatch(addContactAc({contactId}))
+        const { contacts } = e.target
+        let contactId = values2.filter(el => el.label == contacts.value)
+        dispatch(addContactAc({ contactId }))
         // let allUsers = здесь логика из стейта со всеми юзерами :)
-        }
+    }
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const {number, todo} = e.target
+        const { number, todo } = e.target
         let value = number.value.split(': ');
         let contact = user.user.contacts.filter((el) => value[0] === el.name && value[1] === el.phone)[0]
         // let allUsers = здесь логика из стейта со всеми юзерами :)
-        if(!contact) {
-            dispatch(addToDoAc({withContact:false,contactId:'', todo}))
+        if (!contact) {
+            dispatch(addToDoAc({ withContact: false, contactId: '', todo }))
         }
         else {
-            dispatch(addToDoAc({withContact:true,contactId: contact._id, todo}))
+            dispatch(addToDoAc({ withContact: true, contactId: contact._id, todo }))
 
         }
     }
@@ -59,40 +63,40 @@ function ContentPage(props) {
                 <Row>
                     <Col>
                         <Form onSubmit={handleSubmitAddContact}>
-                            <Form.Group  align={'center'}>
+                            <Form.Group align={'center'}>
                                 <Form.Label >Найти контакт по имени</Form.Label>
-                        <Select name={'contacts'}
-                                options={users}
-                                closeOnSelect={true}
-                        />
+                                <Select name={'contacts'}
+                                    options={values2}
+                                    closeOnSelect={true}
+                                />
                             </Form.Group>
                             <Form.Group>
-                            <Button variant="outline-info" size={'sm'} block type="submit">
-                                Add contact
+                                <Button variant="outline-info" size={'sm'} block type="submit">
+                                    Add contact
                             </Button>
                             </Form.Group>
                         </Form>
-                        <Userlist/>
+                        <Userlist />
                     </Col>
                     <Col xs={5}>
                         <Form align={'center'} onSubmit={handleSubmit}>
                             <Form.Group controlId="exampleForm.ControlInput1">
                                 <Form.Label>Enter your todo</Form.Label>
-                                <Form.Control name={'todo'} type="text" placeholder="Write your todo here"/>
+                                <Form.Control name={'todo'} type="text" placeholder="Write your todo here" />
                             </Form.Group>
                             <Form.Group controlId="exampleForm.ControlSelect1">
                                 <Form.Label>Select contact</Form.Label>
                                 <Select name={'number'}
-                                        options={values}
-                                        closeOnSelect={true}
+                                    options={values}
+                                    closeOnSelect={true}
                                 />
                             </Form.Group>
                             <Button variant="outline-info" size={'sm'} block type="submit">
                                 Add to do
                             </Button>
                         </Form>
-                        <ListGroup style={{margin: '2rem'}} align={'center'}>
-                            {user.isLogged&&user.user.todos.map(el=>{
+                        <ListGroup style={{ margin: '2rem' }} align={'center'}>
+                            {user.isLogged && user.user.todos.map(el => {
                                 return <ListGroup.Item>{el.body}</ListGroup.Item>
                             })}
                         </ListGroup>
